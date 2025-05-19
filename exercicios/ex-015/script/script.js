ae591bc830a63a400efc8c014404057162a4f70b
José C. Júnior <lyojunior7@gmail.com>
2025-05-19 11:22:04 -0300

    for in JS

---
+++ exercicios/ex-015/script/script.js
@@ -0,0 +1,17 @@
+var lista = ["Amarelo", "Azul", "Preto", "Verde", "Vermelho"]
+
+var listaUl = document.createElement('ul')
+
+var body = document.getElementsByTagName('body')
+
+body[0].appendChild(listaUl)
+
+var listaNoBody = document.getElementsByTagName('ul')
+console.log(listaNoBody[0]);
+
+for(var i = 0; i < lista.length; i++) {
+    var liFor = document.createElement('li')
+    var textoLi = document.createTextNode(lista[i])
+    liFor.appendChild(textoLi)
+    listaNoBody[0].appendChild(liFor)
+}
